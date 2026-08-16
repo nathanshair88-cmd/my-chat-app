11f9f4806504ab21ad7d4fe55b1f20e31c8bc4e0
Nathan Davies <nathans_hair@hotmail.com>
2026-08-16 21:46:54 +0100

    Fix asyncpg sslmode bug for Neon DB

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,9 +10,14 @@ if DATABASE_URL.startswith("postgres://"):
 elif DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
+if "?sslmode=require" in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("?sslmode=require", "")
+
 connect_args = {}
 if "sqlite" in DATABASE_URL:
     connect_args["check_same_thread"] = False
+elif "postgres" in DATABASE_URL:
+    connect_args["ssl"] = "require"
 
 engine = create_async_engine(
     DATABASE_URL,
